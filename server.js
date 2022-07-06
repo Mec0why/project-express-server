d5331d007ced83ba3d0ba27d52294f58e1356ecc
Mikolaj Renke <mikolaj.renke@gmail.com>
2022-07-06 22:52:19 +0200

    Add endpoints for styles + Photos - Now they will be applied to the Page

--- server.js
+++ server.js
@@ -34,6 +34,14 @@ app.use((req, res) => {
   res.status(404).send('404 not found...');
 });
 
+app.get('/style.css', (req, res) => {
+  res.sendFile(path.join(__dirname, '/style.css'));
+});
+
+app.get('/test.png', (req, res) => {
+  res.sendFile(path.join(__dirname, '/test.png'));
+});
+
 app.listen(8000, () => {
   console.log('Server is running on port: 8000');
 });
